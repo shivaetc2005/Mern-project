53216a092bed9374b7bb9a981f2166aac55f41b6
shivaetc2005 <shivaetc2005@gmail.com>
2026-07-06 11:14:35 +0530

    Added middleware

--- server/src/app.js
+++ server/src/app.js
@@ -2,6 +2,13 @@ const express = require("express");
 
 const app = express();
 
+// Middleware
+app.use((req, res, next) => {
+    console.log(`${req.method} ${req.url}`);
+    next();
+});
+
+// Route
 app.get("/", (req, res) => {
     res.send("Welcome to MERN Backend");
 });
