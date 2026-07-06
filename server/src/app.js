36364c87481cfb097ff6a01ab3cd737b2e797b56
shivaetc2005 <shivaetc2005@gmail.com>
2026-07-06 12:32:44 +0530

    Added routes

--- server/src/app.js
+++ server/src/app.js
@@ -1,21 +1,22 @@
 const express = require("express");
 const cors = require("cors");
 
+const todoRoutes = require("./routes/todoRoutes");
+
 const app = express();
 
-// Built-in Middleware
 app.use(express.json());
 app.use(cors());
 
-// Custom Middleware
 app.use((req, res, next) => {
     console.log(`${req.method} ${req.url}`);
     next();
 });
 
-// Routes
 app.get("/", (req, res) => {
     res.send("Welcome to MERN Backend");
 });
+
+app.use("/api/todos", todoRoutes);
 
 module.exports = app;
