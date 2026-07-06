8d39ca858834d29a0dc6ae4c6701a80c40923e62
shivaetc2005 <shivaetc2005@gmail.com>
2026-07-06 12:02:31 +0530

    Added middlewares

--- server/src/app.js
+++ server/src/app.js
@@ -3,6 +3,17 @@ const cors = require("cors");
 
 const app = express();
 
+// Built-in Middleware
+app.use(express.json());
+app.use(cors());
+
+// Custom Middleware
+app.use((req, res, next) => {
+    console.log(`${req.method} ${req.url}`);
+    next();
+});
+
+// Routes
 app.get("/", (req, res) => {
     res.send("Welcome to MERN Backend");
 });
