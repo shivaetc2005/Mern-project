f750971b0911fbcc8c01b870cb8a74bca0e4693f
shivaetc2005 <shivaetc2005@gmail.com>
2026-07-09 13:43:52 +0530

    Mongob connection

--- server/src/server.js
+++ server/src/server.js
@@ -1,9 +1,13 @@
 require("dotenv").config();
-
+const dns=require('dns');
+dns.setServers(["1.1.1.1", "8.8.8.8"]);
 const app = require("./app");
+const connectDB = require("./config/db");
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
+connectDB().then(() => {
+    app.listen(PORT, () => {
+        console.log(`Server running on http://localhost:${PORT}`);
+    });
 });
