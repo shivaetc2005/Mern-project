b9d103e0c86ba06acb5595fbb74b8208e7570812
shivaetc2005 <shivaetc2005@gmail.com>
2026-07-09 16:46:14 +0530

    Worked with mongodb thru Nodejs application

--- server/src/controllers/todoController.js
+++ server/src/controllers/todoController.js
@@ -1,11 +1,48 @@
-const getTodos = (req, res) => {
-    res.json({
-        success: true,
-        message: "Fetching all todos",
-        data: []
-    });
+const Todo = require("../models/Todo");
+
+const getTodos = async (req, res) => {
+    try {
+
+        const todos = await Todo.find();
+
+        res.status(200).json({
+            success: true,
+            count: todos.length,
+            data: todos
+        });
+
+    } catch (error) {
+
+        res.status(500).json({
+            success: false,
+            message: error.message
+        });
+
+    }
+};
+
+const createTodo = async (req, res) => {
+    try {
+
+        const todo = await Todo.create(req.body);
+
+        res.status(201).json({
+            success: true,
+            message: "Todo created successfully",
+            data: todo
+        });
+
+    } catch (error) {
+
+        res.status(400).json({
+            success: false,
+            message: error.message
+        });
+
+    }
 };
 
 module.exports = {
-    getTodos
+    getTodos,
+    createTodo
 };
--- server/src/routes/todoRoutes.js
+++ server/src/routes/todoRoutes.js
@@ -2,8 +2,13 @@ const express = require("express");
 
 const router = express.Router();
 
-const { getTodos } = require("../controllers/todoController");
+const {
+    getTodos,
+    createTodo,
+} = require("../controllers/todoController");
 
 router.get("/", getTodos);
+
+router.post("/", createTodo);
 
 module.exports = router;
